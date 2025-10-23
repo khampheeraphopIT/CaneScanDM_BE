f655b4a6b17544e2de2400f6a535c94884db99f3
khampheeraphop <poplowplay1@gmail.com>
2025-10-23 16:50:18 +0700

    fix 500

--- src/config.py
+++ src/config.py
@@ -4,13 +4,13 @@ load_dotenv()
 
 class Settings:
     BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    UPLOAD_FOLDER = os.path.join(os.getcwd(), "uploads")
-    GRAD_CAM_FOLDER = os.path.join(os.getcwd(), "src", "gradcam")
+    UPLOAD_FOLDER = os.path.join(BASE_DIR, "uploads")
+    GRAD_CAM_FOLDER = os.path.join(BASE_DIR, "src", "gradcam")
     MODEL_PATH = os.getenv(
         "MODEL_PATH",
         os.path.join(BASE_DIR, "src", "model", "model", "model_20251019_122736.pth")
     )
-    CSV_LOG_PATH = os.path.join(os.getcwd(), "prediction_logs.csv")
+    CSV_LOG_PATH = os.path.join(BASE_DIR, "prediction_logs.csv")
     ALLOW_ORIGINS = os.getenv(
         "ALLOW_ORIGINS",
         "http://localhost:5173,http://10.0.2.2:8000"
